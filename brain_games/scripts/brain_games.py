baa2c59cbc39c08bf33598a32d339b5dc49f0302
Dmitry Kabanov <dkabanov6@gmail.com>
2022-11-15 20:24:44 +0500

    updated brain games file

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import prompt
+
 
 def main():
     print('Welcome to the Brain Games!')
+
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}')
 
 
 if __name__ == '__main__':
